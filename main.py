1ef8b8eaa148dfc751ece117e0de1d96249018af
Akisoy-Zenash <109104482+Akisoy-Zenash@users.noreply.github.com>
2023-01-07 22:55:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,29 @@ from pytube import YouTube
 from pytube import Playlist
 import os
 
+#la playlist que je veut 
 plist = Playlist("https://youtube.com/playlist?list=PLlKPfDQ7VPGG7FTto-Igt03DO2MZOTTu3")
+#le dossier ou je vais la mettre
 destination = "C:/Users/akiso/Downloads/py_musique"
 
-
+#recup url de chaque vid et downlowd
 for url in plist:
+    #affiche url
     print(url)
+
+    #prendre que l'audio
     vid = YouTube(url)
     audio = vid.streams.filter(only_audio=True).first()
     out_file = audio.download(output_path=destination)
 
+    #ctrl+c ctrl+v jsp trop se que ca fait de se que je voit ca convertie .mp3 et save dans le bon dossier
     base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
   
-
+    #afficher si tout va bien
     print(vid.title + " has been successfully downloaded.")
 
+#afficher fin
 print("All Successfully downloaded")
+
